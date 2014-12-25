94ff5d00f87534e355c1c446aad8039600e384f5
Russell Briggs <russ@paraflyer.net>
2014-12-25 01:06:44 +0000

    Update to settings.py

--- settings.py
+++ settings.py
@@ -6,22 +6,24 @@ DEBUG_SERVER_PORT = 8888
 DEBUG_LOG_REQUESTS = True
 
 DATABASES = {
-	'default' : {
-		'provider' : 'rev.db.providers.mongodb',
-		'host' :  '127.0.0.1',
-		'port' : 27017,
-		'database' : "revtest",
-	}
+    'default' : {
+        'provider' : 'rev.db.providers.mongodb',
+        'host' :  '127.0.0.1',
+        'port' : 27017,
+        'database' : "revtest",
+    }
 }
 
 MODULE_PATHS = [
-	'/home/russell/Code/revframework/rev_modules',
-	'/home/russell/Code/revcrm/rev_modules',
+    '/home/russell/code/revframework/revframework/rev_modules',
+    '/home/russell/code/revframework/revcrm/rev_modules',
 ]
 
 INSTALLED_MODULES = [
-	'rev_app',
-	'revcrm_base',
+    'rev_app',
+    'rev_auth',
+    'revcrm_base',
+    'revcrm_calendar',
 ]
 
 from site_settings import *
